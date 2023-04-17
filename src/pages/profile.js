3d4204488c25858d06020f88e4ed9349b16bb971
DishaAgarwalla <dishaagarwalla2001@gmail.com>
2023-04-17 15:18:12 +0530

    link added to redeem page in tokenids in profile

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -15,6 +15,7 @@ import {
 } from "wagmi";
 import { getConfig } from "../config/config";
 import ticketAbi from "../ethereum/build/TicketAbi.json";
+import { Link } from "react-router-dom";
 
 export const Container = styled.div`
   display: flex;
@@ -189,10 +190,10 @@ const Profile = () => {
         <TicketDisplayContainer>
           {data?.[0]?.map((tokenId) => {
             return (
-              <div key={tokenId}>
+              <Link to={`/redeem/${tokenId}`} key={tokenId}>
                 <img src={TicketPlaceholder} className="ticket" />
                 <div>#{tokenId.toString()}</div>
-              </div>
+              </Link>
             );
           })}
           {/* <img src={TicketPlaceholder} className="ticket" />
