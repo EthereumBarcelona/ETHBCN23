aeff5b0587ea33e371a1fb79836de0762840ecee
DishaAgarwalla <dishaagarwalla2001@gmail.com>
2023-05-03 01:09:57 +0530

    new contracts on sepolia

--- src/config/config.js
+++ src/config/config.js
@@ -14,7 +14,7 @@ export const getConfig =
         network: sepolia,
         alchemyKey: process.env.REACT_APP_ALCHEMY_API_KEY,
         alchemyUrl: `https://eth-sepolia.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_API_KEY}`,
-        ticketContractAddress: "0xc655BeE055E1F1caAA3ECbadA72d4987932377ED",
-        usdcAddress: "0x66E1DFa5685546a559596Fc2295c82B95f3fDF02",
+        ticketContractAddress: "0xB378f24Ccb592B94c148bD1F7Bcb628c37e98058",
+        usdcAddress: "0xBe473174D3913A13Ce80C157e685b994ad1c17C8",
         explorerUrl: "https://sepolia.etherscan.io",
       };
