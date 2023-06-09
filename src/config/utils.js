4466c1bc21c541a49d6f8acc1c15d7268b7511e4
lladawn <dishaagarwalla2001@gmail.com>
2023-06-10 00:55:27 +0530

    mainnet config

--- src/config/utils.js
+++ src/config/utils.js
@@ -1,45 +1,12 @@
 // mainnet polygon
-// export const wertDetails = {
-//   origin: "https://widget.wert.io",
-//   partnerId: process.env.REACT_APP_WERT_PARTNER_ID,
-//   contractAddress: "0x25e302dF0d301AF5874Fd3C7B461d46be60473dF",
-//   network: "polygon",
-//   commodity: "USDC",
-//   ticketPrice: 444, //usdc on polygon
-//   mintAbi: {
-//     inputs: [
-//       {
-//         internalType: "uint256",
-//         name: "_waveNum",
-//         type: "uint256",
-//       },
-//       {
-//         internalType: "address",
-//         name: "_account",
-//         type: "address",
-//       },
-//       {
-//         internalType: "uint256",
-//         name: "_numberOfTokens",
-//         type: "uint256",
-//       },
-//     ],
-//     name: "mintTicketTo",
-//     outputs: [],
-//     stateMutability: "payable",
-//     type: "function",
-//   },
-// };
-
-// testnet
 export const wertDetails = {
-  origin: "https://sandbox.wert.io", // https://widget.wert.io
+  origin: "https://widget.wert.io",
   partnerId: process.env.REACT_APP_WERT_PARTNER_ID,
   privateKey: process.env.REACT_APP_WERT_PRIVATE_KEY,
-  contractAddress: "0x25e302dF0d301AF5874Fd3C7B461d46be60473dF",
-  network: "goerli",
-  commodity: "ETH",
-  ticketPrice: 0.001, //usdc on polygon
+  contractAddress: "0x6052ed5C646574D12c27E8D219C49C3394598b00",
+  network: "polygon",
+  commodity: "USDC",
+  ticketPrice: 444, //usdc on polygon
   mintAbi: {
     inputs: [
       {
@@ -64,3 +31,37 @@ export const wertDetails = {
     type: "function",
   },
 };
+
+// testnet
+// export const wertDetails = {
+//   origin: "https://sandbox.wert.io", // https://widget.wert.io
+//   partnerId: process.env.REACT_APP_WERT_PARTNER_ID,
+//   privateKey: process.env.REACT_APP_WERT_PRIVATE_KEY,
+//   contractAddress: "0x25e302dF0d301AF5874Fd3C7B461d46be60473dF",
+//   network: "goerli",
+//   commodity: "ETH",
+//   ticketPrice: 0.001, //usdc on polygon
+//   mintAbi: {
+//     inputs: [
+//       {
+//         internalType: "uint256",
+//         name: "_waveNum",
+//         type: "uint256",
+//       },
+//       {
+//         internalType: "address",
+//         name: "_account",
+//         type: "address",
+//       },
+//       {
+//         internalType: "uint256",
+//         name: "_numberOfTokens",
+//         type: "uint256",
+//       },
+//     ],
+//     name: "mintTicketTo",
+//     outputs: [],
+//     stateMutability: "payable",
+//     type: "function",
+//   },
+// };
