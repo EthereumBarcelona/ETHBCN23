4d2a12f534c736d0cac4037dd8644944ee4d5352
lladawn <dishaagarwalla2001@gmail.com>
2023-06-29 00:19:08 +0530

    organizer page integrations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -18,11 +18,13 @@ import {
   lightTheme,
   Theme,
 } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { configureChains, createClient, useAccount, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { getConfig } from "./config/config";
 import QR from "./pages/qrcode";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import axios from "axios";
 
 const { chains, provider } = configureChains(
   [...getConfig.networks],
@@ -41,6 +43,25 @@ const wagmiClient = createClient({
 });
 
 function App() {
+  const { address } = useAccount();
+  const [isOrganizer, setIsOrganizer] = useState(false);
+
+  useEffect(() => {
+    const checkIfOrganizer = async () => {
+      // address === "0x36a165d9e8084D8788a5494Aa022035c7dE5FF3D";
+
+      const {
+        data: { isOrganizer },
+      } = await axios({
+        method: "GET",
+        url: `${getConfig.apiBaseUrl}/checkOrganizer/${address}`,
+      });
+      console.log(isOrganizer);
+      setIsOrganizer(isOrganizer);
+    };
+    checkIfOrganizer();
+  }, [address]);
+
   return (
     <div className="App">
       <Router>
@@ -61,7 +82,16 @@ function App() {
             <Routes>
               <Route exact path="/" element={<Profile />} />
               <Route exact path="/mint" element={<Mint />} />
-              <Route exact path="/scan" element={<Scan />} />
+              {/* <Route exact path="/scan" element={<Scan />} /> */}
+              <Route
+                exact
+                path="/scan"
+                element={
+                  <ProtectedRoute permit={isOrganizer}>
+                    <Scan account={address} orgId={"testing"} />
+                  </ProtectedRoute>
+                }
+              />
               <Route
                 exact
                 path="/redeem/:chainName/:chainId/:tokenId/:ticketId"
